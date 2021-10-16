5505403677fef099630f306d0afaac8ddae8e967
Niklas Vogel <niklbird@hotmail.com>
2021-10-16 17:49:48 +0200

    Adding comments

--- src/utils.py
+++ src/utils.py
@@ -21,31 +21,13 @@ import datum
 import datetime
 import mushroom
 
-
-def preprocess(data):
-    # Find middle point for all 5 point surfaces
-    new_data = []
-    for i in range(0, len(data), 5):
-        try:
-            point = data[i]
-            x_1 = data[i]["geometry"]["coordinates"][0]
-            y_1 = data[i]["geometry"]["coordinates"][1]
-            x_2 = data[i + 2]["geometry"]["coordinates"][0]
-            y_2 = data[i + 1]["geometry"]["coordinates"][1]
-            x_mid = x_1 + 0.5 * (x_2 - x_1)
-            y_mid = y_1 + 0.5 * (y_2 - y_1)
-            point["geometry"]["coordinates"] = (x_mid, y_mid)
-            new_data.append(point)
-        except Exception:
-            print("Exception")
-    return new_data
-
-
 p = Proj("EPSG:5683")
 p2 = Proj("EPSG:3034")
 
 
 def plt_shapefile(shapes):
+    # Plot shapefile
+    # Currently not used
     map = Basemap(width=1200000, height=900000, resolution=None, projection='lcc', lat_0=51, lon_0=10.5)
     # draw coastlines, country boundaries, fill continents.
     map.bluemarble()
@@ -72,27 +54,30 @@ def translate_epsg2_to_utm(coord):
 
 @jit(nopython=True)
 def get_lat_fac():
-    # translate angle distance to km distance latitudial
+    # Translate angle distance to km distance latitudial
     return 110.574
 
 
 @jit(nopython=True)
 def get_long_fac(longitude):
-    # translate angle distance to km distance longitudinal
+    # Translate angle distance to km distance longitudinal
     return np.abs(111.32 * np.cos(longitude))
 
 
 @jit(nopython=True)
 def get_distance(x_1, y_1, x_2, y_2):
+    # Get distance in km between two coordinates
     return np.sqrt(((x_1 - x_2) * get_lat_fac()) ** 2 + ((y_1 - y_2) * get_long_fac(x_1)) ** 2)
 
 
 def get_distance_arr(x_1, y_1, x_2, y_2):
+    # Get distance for an entire array in km
     return np.sqrt(((x_1 - x_2) * get_lat_fac()) ** 2 + ((y_1 - y_2) * get_long_fac(x_1)) ** 2)
 
 
 @jit(nopython=True)
 def find_closest_station(coord, stations):
+    # Find the closest DWD station to a coordinate
     best_dist = np.float64(100000.0)
     best_stat = np.int8(0)
     for i in range(len(stations)):
@@ -112,11 +97,12 @@ def get_german_treename(latname):
 
 
 def get_latname_treename(germname):
-    # translate german treename to JSON name
+    # Translate german tree name to JSON name
     return constants.treeNames_g[germname]
 
 
 def create_points_inner(topx, topy, botx, boty, x_add, y_add, dist):
+    # Create points inside of a patch
     cords = []
     cord = [topx, topy]
     while cord[1] + y_add < boty:
@@ -129,7 +115,9 @@ def create_points_inner(topx, topy, botx, boty, x_add, y_add, dist):
     return cords
 
 
-def create_points(topx, topy, botx, boty, dist, batch_size_sqrt):
+def create_points(topx, topy, botx, boty, dist, patch_size_sqrt):
+    # Create points from topx to boty with equal distance dist
+    # Combine them in patches of batch_size
     print("Starting to create points")
     x_start = min(topx, botx)
     y_start = min(topy, boty)
@@ -149,25 +137,25 @@ def create_points(topx, topy, botx, boty, dist, batch_size_sqrt):
         stations_minimized.append([station['geo_lat'], station['geo_lon'], station['station_id']])
     stations_minimized = np.array(stations_minimized, dtype=np.float64)
 
-    while cord[1] + batch_size_sqrt * y_add < y_end:
-        while cord[0] + batch_size_sqrt * x_add < x_end:
+    while cord[1] + patch_size_sqrt * y_add < y_end:
+        while cord[0] + patch_size_sqrt * x_add < x_end:
             batch = create_points_inner(cord[0], cord[1], cord[0] +
-                                        batch_size_sqrt * x_add, cord[1]
-                                        + batch_size_sqrt * y_add, x_add, y_add, dist)
+                                        patch_size_sqrt * x_add, cord[1]
+                                        + patch_size_sqrt * y_add, x_add, y_add, dist)
 
-            middle = get_middle(cord[0], cord[0] + batch_size_sqrt * x_add, cord[1], cord[1] + batch_size_sqrt * y_add)
+            middle = get_middle(cord[0], cord[0] + patch_size_sqrt * x_add, cord[1], cord[1] + patch_size_sqrt * y_add)
 
             station_id = find_closest_station(np.array(cord), stations_minimized)
 
-            corners = create_corners(cord, batch_size_sqrt * x_add, batch_size_sqrt * y_add)
+            corners = create_corners(cord, patch_size_sqrt * x_add, patch_size_sqrt * y_add)
 
             patches.append(patch.Patch(batch, middle, station_id, corners))
-            cord = [cord[0] + batch_size_sqrt * x_add, cord[1]]
+            cord = [cord[0] + patch_size_sqrt * x_add, cord[1]]
 
-        cord = [x_start, cord[1] + batch_size_sqrt * y_add]
+        cord = [x_start, cord[1] + patch_size_sqrt * y_add]
 
     print("Created amount of batches: " + str(len(patches)))
-    print("Created amount of points: " + str(len(patches) * (batch_size_sqrt ** 2)))
+    print("Created amount of points: " + str(len(patches) * (patch_size_sqrt ** 2)))
     return patches
 
 
@@ -181,6 +169,7 @@ def get_middle(x_start, x_end, y_start, y_end):
 
 
 def calc_averages(shape_points):
+    # Calc averages of points
     res = []
     for i in range(len(shape_points)):
         points = np.array(shape_points[i])
@@ -189,12 +178,14 @@ def calc_averages(shape_points):
 
 
 def find_closest_point(point, points):
+    # Currently not used
     arr = np.array([abs(get_distance(point[0], point[1], points[i][0], points[i][1])) for i in range(len(points))])
     min_ind = np.argmin(arr)
     return min_ind, points[min_ind]
 
 
 def find_n_closest_points(point, points):
+    # Currently not used
     arr = np.array([abs(get_distance(point[0], point[1], points[i][0], points[i][1])) for i in range(len(points))])
     arr_sorted = np.sort(arr)
     return np.where(arr == arr_sorted[0]), np.where(arr == arr_sorted[1]), \
@@ -202,17 +193,21 @@ def find_n_closest_points(point, points):
 
 
 def project_coordinate_inverse(coordinate, projection):
+    # Use inverse projection on coordinate
     p = Proj(projection)
     point = p(coordinate[0], coordinate[1], inverse=True)
     return [point[1], point[0]]
 
 
 def project_coordinate(coordinate, projection):
+    # Use projection on coordinate
     p = Proj(projection)
     return p(coordinate[0], coordinate[1])
 
 
 def project_shapes(shapes: list, projection: str):
+    # If shapes are not stored in correct coordinate system
+    # Project them to common coordinate system
     re = []
     finished_shapes = 0
     print("Start projecting %f shapes", len(shapes))
@@ -235,11 +230,13 @@ def create_lookup(shape_folder):
 
 
 def create_records(shape_folder):
+    # Create list of records (data about each shape)
     sf = shapefile.Reader(shape_folder, encoding="iso-8859-1")
     return sf.records()
 
 
 def parse_in_shape(shape_folder, projection):
+    # Parse in a shape file
     sf = shapefile.Reader(shape_folder, encoding="iso-8859-1")
     return project_shapes(sf.shapes(), projection), sf.records(), create_lookup(shape_folder)
 
@@ -283,24 +280,22 @@ def patch_in_shape(shape, patch):
 
 def cut_patches(patches, shape):
     # Ensure that patches only contain points in shape
+    # Used to remove all points that are not in a shape
+    # E.g. remove points that are not in Germany from all patches that lie on the border
     for patch in patches:
         patch_in_shape(np.array(shape), patch)
 
 
-def extract_treeinfo_for_point(point, records, patch_ll):
-    trees = {}
-    trees['hardwood'] = records['pDecid']
-    trees['softwood'] = records['pConifer']
-    trees['coverage'] = records['pAll']
-    trees['buche'] = records['pFagus']
-    trees['kiefer'] = records['pPinus']
-    trees['fichte'] = records['pPicea']
-    trees['eiche'] = records['pQuerus']
-    trees['birke'] = records['pBetula']
+def extract_tree_info_for_point(point, records, patch_ll):
+    # Extract all tree data relevant for mushrooms
+    trees = {'hardwood': records['pDecid'], 'softwood': records['pConifer'], 'coverage': records['pAll'],
+             'buche': records['pFagus'], 'kiefer': records['pPinus'], 'fichte': records['pPicea'],
+             'eiche': records['pQuerus'], 'birke': records['pBetula']}
     patch_ll.dates.append(datum.Datum(point, trees, 0))
 
 
 def fit_trees_to_patch(tree_middlepoints, tree_records, patches, patch_size):
+    # Put all tree shapes into patch that have a middle-point inside of this patch
     for patch_l in patches:
         points = np.array(patch_l.points)
         middle = np.array(patch_l.middle)
@@ -315,7 +310,7 @@ def fit_trees_to_patch(tree_middlepoints, tree_records, patches, patch_size):
                 # [0][0] required as this returns a nested array
                 smallest = np.argwhere(dist == np.amin(dist))[0][0]
                 record = tree_records[args[smallest][0]]
-                extract_treeinfo_for_point(point, record, patch_l)
+                extract_tree_info_for_point(point, record, patch_l)
 
 
 def middle_points(points):
@@ -323,6 +318,7 @@ def middle_points(points):
 
 
 def filter_relevant_weather_data(weather_data):
+    # Only use weather data relevant to mushrooms
     if weather_data == None:
         return
     ret = {}
@@ -333,10 +329,12 @@ def filter_relevant_weather_data(weather_data):
 
 
 def format_timestamp(timestamp_l):
+    # Format timestamp for DWD request
     return datetime.datetime(timestamp_l.year, timestamp_l.month, timestamp_l.day, 12)
 
 
 def add_weather(patches):
+    # Add weather data to each patch
     for patch_l in patches:
         patch_l.weather_data = {}
         weather = patch_l.weather_data
@@ -356,6 +354,7 @@ def add_weather(patches):
 
 
 def get_month_factors(month):
+    # Factor that indicates if mushroom is in season
     ret = {}
     mushroooms = mushroom.readXML()
     for s_name in mushroooms.keys():
@@ -365,6 +364,7 @@ def get_month_factors(month):
 
 
 def calc_dynamic_value(patches):
+    # Calculate the actual mushroom probabilities
     month_facs = get_month_factors(datetime.datetime.today().month)
     for patch in patches:
         weather = patch.weather_data
@@ -397,7 +397,7 @@ def calc_static_values(patches):
 def reparse(cut_out_germany):
     patches = create_points(50.00520532919058, 8.646406510673339, 49.767632303668734, 9.118818592516165, 0.1, 10)
     # If some of the points can be outside of Germany -> Only use points inside
-    if(cut_out_germany):
+    if cut_out_germany:
         germany_shape = io_utils.read_dump_from_file("C:/Users/Niklas/Desktop/GIT/shrooms/data/ger_folder/ger_points_proc.dump")[1]
         cut_patches(patches, germany_shape)
     trees = io_utils.read_dump_from_file("C:/Users/Niklas/Desktop/GIT/shrooms/data/trees_folder/trees_points_proc.dump")
